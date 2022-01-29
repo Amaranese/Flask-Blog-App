ce437f15ff5f45c7f926e4d791cfc60728b86665
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-29 04:19:26 +0100

    .

---
+++ FLASK_BLOG/flaskblog.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+	return "<h1>Greetings turd burglars</h1>"
+
+@app.route("/about")
+def about():
+	return "<h1>About Page</h1>"
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
